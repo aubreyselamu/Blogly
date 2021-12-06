a926645e8b0b24c2e1618d374a406a92084bd3de
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-12-06 11:56:13 -0500

    added property under Post model to adjust date,time

--- models.py
+++ models.py
@@ -18,6 +18,8 @@ class User(db.Model):
     last_name = db.Column(db.Text, nullable = False)
     image_url = db.Column(db.Text, nullable = False, default= "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png")
 
+    posts = db.relationship('Post', backref='user')
+
 class Post(db.Model):
     '''Post'''
 
@@ -28,3 +30,7 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable = False)
     created_at = db.Column(db.DateTime, nullable = False, default = datetime.datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable =False)
+
+    @property
+    def friendly_date(self):
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
