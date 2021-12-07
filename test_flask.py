da71a436fb83b3b208f541951183fb6400afbf20
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-12-07 10:30:26 -0500

    added additional tests for Post route

--- test_flask.py
+++ test_flask.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from app import app
-from models import db, User
+from models import db, User, Post
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_ECHO'] = False
@@ -63,3 +63,61 @@ class UserViewsTestCase(TestCase):
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn('First Name', html)
+    
+    
+    class PostViewsTestCase(TestCase):
+        '''Tests for Post Model'''
+
+    def setUp(self):
+        """Add sample user."""
+
+        Post.query.delete()
+        
+
+        user = User(first_name="TestName", last_name="Working", image_url="https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png")
+        post = Post(title="TestName", content="It Is Working", user_id = 1 )
+        
+        db.session.add(user)
+        db.session.commit()
+
+        db.session.add(post)
+        db.session.commit()
+
+        self.user_id = user.id
+        self.post_id = post.id
+        
+
+    def tearDown(self):
+        """Clean up any fouled transaction."""
+
+        db.session.rollback()
+    
+    def test_show_post_form(self):
+        with app.test_client() as client:
+            resp = client.get(f"/users/{self.user_id}/posts/new")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Add Posts for', html)
+    
+    def test_show_post(self):
+        with app.test_client() as client:
+            resp = client.get(f"/posts/{self.post_id}")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('It Is Working', html)
+    
+    def test_show_edit_form(self):
+        with app.test_client() as client:
+            resp = client.get(f"/posts/{self.post_id}/edit", follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('TestName', html)
+    
+
+            
+            
+                
+    
