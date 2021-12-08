0d7b3dcdf664342f3b1e9bc884a3738055543db9
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-12-08 10:18:14 -0500

    added more objects to session for Tag and PostTag models

--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-from models import User, Post, db 
+from models import User, Post, Tag, PostTag, db 
 from app import app
 
 db.drop_all()
@@ -35,6 +35,44 @@ fish = Post(title = 'Fishing in Homosassa, FL', content='My first fishing experi
 db.session.add(engineering)
 db.session.add(soccer)
 db.session.add(fish)
+
+#Commit to session
+db.session.commit()
+
+#********************************************************
+#Tag Model
+
+#If tags table isn't empty, empty it
+Tag.query.delete()
+
+#Add posts
+software_tag = Tag(name='software engineering')
+soccer_tag = Tag(name='soccer')
+fish_tag = Tag(name='fishing')
+
+#Add new objects to session
+db.session.add(software_tag)
+db.session.add(soccer_tag)
+db.session.add(fish_tag)
+
+#Commit to session
+db.session.commit()
+
+#********************************************************
+#PostTag Model
+
+#If tags table isn't empty, empty it
+PostTag.query.delete()
+
+#Add posts
+software_pt = PostTag(post_id=1, tag_id=1)
+soccer_pt = PostTag(post_id=2, tag_id=2)
+fish_pt = PostTag(post_id=3, tag_id=3)
+
+#Add new objects to session
+db.session.add(software_pt)
+db.session.add(soccer_pt)
+db.session.add(fish_pt)
 
 #Commit to session
 db.session.commit()
