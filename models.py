3b887596dcf6e3df4c062d3c81acb225854d7b21
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-12-08 10:17:46 -0500

    added Tag and PostTag Model, established relationship between tables

--- models.py
+++ models.py
@@ -34,3 +34,21 @@ class Post(db.Model):
     @property
     def friendly_date(self):
         return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+class Tag(db.Model):
+    '''Tag'''
+
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, unique=True, nullable=False)
+
+    posts = db.relationship('Post', secondary='posts_tags', backref='tags')
+
+class PostTag(db.Model):
+    '''Post Tag'''
+
+    __tablename__ = 'posts_tags'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'),primary_key=True)
